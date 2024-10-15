38d1b74a23c42d33e6b56cd1b8bab2c0d712753c
Yovany Cunha <yovany33@gmail.com>
2024-10-14 22:37:12 -0300

    fix output da média cras

--- aluno.hs
+++ aluno.hs
@@ -1,3 +1,15 @@
+{-
+5 - Um aluno é representado como uma estrutura contando matrícula,
+um primeiro nome, um sobrenome, período de ingresso e CRA.
+
+a - Implemente a estrutura que representa um aluno
+b - Implemente uma função que calcula a média dos CRAs dos alunos (dispostos em uma lista)
+usando o operador de foldr (você não pode usar map)
+c - Implemente uma função que realiza o groupBy dos alunos por CRA.
+Ou seja, dada uma lista de alunos, a função retorna uma lista de pares (cra, [Aluno]), agrupando alunos 
+com um mesmo CRA em pares cujo primeiro elemento é o CRA e o segundo é uma lista de alunos.
+-}
+
 module Aluno (
     Aluno(..),
     getCra
